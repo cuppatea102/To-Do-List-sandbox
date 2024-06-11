eca15ec212364f6711c8adf71e4e744404dfafc7
windowlicker <wadetownley2000@gmail.com>
2024-06-11 15:51:30 -0600

    Added Css cases and changed some wording

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ document.getElementById('addTodo').addEventListener('click', function() {
     if (newTodoText.trim() !== '') {
       // Create a new list item
       var newListItem = document.createElement('li');
+      newListItem.id = 'lists'
       // <li></li>
 
      var newDeleteButton = document.createElement('button');
       newDeleteButton.id = 'delete';
       newDeleteButton.onclick = deleteToDo;
-      newDeleteButton.appendChild(document.createTextNode('Remove Item'));
-      //<button id="delete" onclick = "deleteToDo(event)">'Remove Item'</button>
+      newDeleteButton.appendChild(document.createTextNode('X'));
+      //<button id="delete" onclick = "deleteToDo(event)">'X'</button>
 
       // Create a new label
       var newLabel = document.createElement('label');
+      newLabel.id = 'store'
       // <label></label>
   
       // Create a new checkbox
@@ -31,11 +33,11 @@ document.getElementById('addTodo').addEventListener('click', function() {
       //newLabel = <label><input type="checkbox" name="To-Do">'example'</label>
 
       newListItem.appendChild(newDeleteButton);
-      //<li><button id="delete" onclick = "deleteToDo(event)">'Remove Item'</button></li>
+      //<li><button id="delete" onclick = "deleteToDo(event)">'X'</button></li>
     
       // Append the label to the list item
       newListItem.appendChild(newLabel);
-      // <li><label><input type="checkbox" name="To-Do">'example'</label><li><button id="delete" onclick = "deleteToDo(event)">'Remove Item'</button></li></li>
+      // <li><label><input type="checkbox" name="To-Do">'example'</label><li><button id="delete" onclick = "deleteToDo(event)">'X'</button></li></li>
   
       // Append the list item to the to-do list
       document.getElementById('todoList').appendChild(newListItem);
@@ -56,8 +58,7 @@ document.getElementById('addTodo').addEventListener('click', function() {
     const isChecked = event.target.checked;
  isChecked ? event.target.closest('li').style.textDecoration = 'line-through' : event.target.closest('li').style.textDecoration = 'none';
   };
-  //creates a line through the list element when checkbox is checked and reverts it when 
-  // unchecked
+  //creates a line through the list element when checkbox is checked and reverts it when unchecked
 
 
  function deleteToDo(event) {
