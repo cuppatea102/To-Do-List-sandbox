d18b676c4f7941c8e37a75db8c297c8abbfb47bb
cuppatea <thomas.avende@gmail.com>
2024-06-25 16:58:16 -0400

    work

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-document.getElementById('addTodo').addEventListener('click', function() {
+document.getElementById('addPokemon').addEventListener('click', function() {
     var newTodoText = document.getElementById('newTodo').value;
     //text box value
     
     if (newTodoText.trim() !== '') {
       // Create a new list item
       var newListItem = document.createElement('li');
-      newListItem.id = 'lists'
+      newListItem.id = 'team'
       // <li></li>
 
      var newDeleteButton = document.createElement('button');
@@ -16,19 +16,12 @@ document.getElementById('addTodo').addEventListener('click', function() {
 
       // Create a new label
       var newLabel = document.createElement('label');
-      newLabel.id = 'store'
+      newLabel.id = 'pokemon'
+      //id formerly 'store'
       // <label></label>
   
-      // Create a new checkbox
-      var newCheckbox = document.createElement('input');
-      newCheckbox.type = 'checkbox';
-      newCheckbox.name = 'To-Do';
-      newCheckbox.onclick = changeOnChecked;
-      //newCheckbox = <input type="checkbox" name="To-Do">
-  
-      // Append the checkbox and text to the label
-      newLabel.appendChild(newCheckbox);
-      //<label><input type="checkbox" name="To-Do"></label>
+
+      //Puts pokemon name in the list
       newLabel.appendChild(document.createTextNode(' ' + newTodoText));
       //newLabel = <label><input type="checkbox" name="To-Do">'example'</label>
 
@@ -40,7 +33,7 @@ document.getElementById('addTodo').addEventListener('click', function() {
       // <li><label><input type="checkbox" name="To-Do">'example'</label><li><button id="delete" onclick = "deleteToDo(event)">'X'</button></li></li>
   
       // Append the list item to the to-do list
-      document.getElementById('todoList').appendChild(newListItem);
+      document.getElementById('fullTeam').appendChild(newListItem);
     //  <ul id="todoList">
     //<li><label id="store"><input type="checkbox" id="store" name="To-Do" value="store" />Go to store</label></li>
     //<li><label id="store"><input type="checkbox" id="cookware" name="To-Do" value="cookware" onclick='changeOnChecked()'>Gather mixing bowls, measuring cups, and measuring spoons</label></li>
@@ -54,11 +47,7 @@ document.getElementById('addTodo').addEventListener('click', function() {
     }
   });
 
-  function changeOnChecked(event) {
-    const isChecked = event.target.checked;
- isChecked ? event.target.closest('li').style.textDecoration = 'line-through' : event.target.closest('li').style.textDecoration = 'none';
-  };
-  //creates a line through the list element when checkbox is checked and reverts it when unchecked
+
 
 
  function deleteToDo(event) {
